f29889314f9ebba762e26ec3db59526db6ecd72f
Frank <stylefk1218@gmail.com>
2024-06-22 17:31:12 +0800

    u

--- Flask/db/mysql_db.py
+++ Flask/db/mysql_db.py
@@ -3,9 +3,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 username = 'root'
 password = 'FRANKWUfrankwu'
-# host = '172.18.0.2'
-host = '192.168.64.4'
-port = '4360'
+# host = '172.18.0.3'
+host = '192.168.64.6'
+port = '4306'
 database = 'test'
 url = f"mysql+mysqlconnector://{username}:{password}@{host}:{port}/{database}"
 engine = create_engine(url, pool_size=5)
